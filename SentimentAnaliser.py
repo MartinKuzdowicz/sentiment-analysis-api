a711e4e8bee44a88704a238c81a6fa764b507faa
Kuzdowicz, Martin (Developer) <martin.kuzdowicz@sky.uk>
2017-03-22 21:46:44 +0000

    refactoring into a class

---
+++ SentimentAnaliser.py
@@ -0,0 +1,42 @@
+from nltk.tokenize import sent_tokenize, word_tokenize
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import BernoulliNB
+import sys  
+import os
+
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+def _getFilePath(filename):
+	return os.path.join(os.path.dirname(__file__), 'resources/training_data/'+filename)
+
+class SentimentAnaliser:
+
+	def __init__(self):
+		with open(_getFilePath('imdb_labelled.txt'), 'r') as text_file:
+		    lines = text_file.read().split('\n')
+		with open(_getFilePath('amazon_cells_labelled.txt'), 'r') as text_file:
+		    lines += text_file.read().split('\n')
+		with open(_getFilePath('yelp_labelled.txt'), 'r') as text_file:
+		    lines += text_file.read().split('\n')
+
+		lines = [line.split('\t') for line in lines if len(line.split('\t'))==2 and line.split('\t')[1] <> '']
+
+		train_documents = [line[0] for line in lines]
+		train_labels = [int(line[1]) for line in lines]
+
+		self.count_vectorizer = CountVectorizer(binary='true')
+		self.count_vectorizer._validate_vocabulary()
+		train_documents = self.count_vectorizer.fit_transform(train_documents)
+
+		self.classifier = BernoulliNB().fit(train_documents,train_labels)
+
+	def getSentimentWeigth(self, text):
+		return self.classifier.predict(self.count_vectorizer.transform([text]))
+
+senAnl = SentimentAnaliser()
+
+example_text = "this is the best movied"
+
+print(senAnl.getSentimentWeigth(example_text))
+
